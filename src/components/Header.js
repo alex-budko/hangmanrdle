f0e61ca44283eb05014b32545cb9ec034fe058f3
alex-budko <xalex.budko@gmail.com>
2022-05-01 14:38:31 -0400

    Added a header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+        <h1>
+            Hangordle
+        </h1>
+        <p>
+            Find the secret word -- Type In a Letter!
+        </p>
+    </>
+  )
+}
+
+export default Header
