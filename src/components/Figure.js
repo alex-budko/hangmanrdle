21568276f7f9540640a0069c622c9e58342244bc
alex-budko <xalex.budko@gmail.com>
2022-05-06 22:07:23 -0400

    svg import

--- src/components/Figure.js
+++ src/components/Figure.js
@@ -3,6 +3,7 @@ import React from 'react'
 //svg figure imported into website
 const Figure = () => {
   return (
+    //svg import 
     <svg height="250" width="200" class="figure-container">
         
          {/* Rod */}
