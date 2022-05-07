a376ef77f33dc87beffb6b71209eb5a053610515
alex-budko <xalex.budko@gmail.com>
2022-05-07 15:05:25 -0400

    implemented wrong letters

--- src/components/Word.js
+++ src/components/Word.js
@@ -4,15 +4,15 @@ import React from 'react'
 const Word = ({ selectedWord, correctLetters }) => {
   return (
 
-    <div className='word' id='word'>
+    <div className='word'>
       {selectedWord.split('').map((letter, i) => {
         return (
           <span className="letter" key={i}>
             {correctLetters.includes(letter) ? letter : ''}
           </span>
 			)})}
-
     </div>
+
   );
 }
 
